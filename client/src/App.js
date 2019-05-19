971bfcac049b4e088d00966c1d17d25b8142e1c4
bhargav00 <bmaniar00@gmail.com>
2019-05-19 12:06:06 +0530

    adding topBar and getting initial values from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
 import LoadingOverlay from 'react-loading-overlay';
-import 'babel-polyfill';
-import './App.scss';
 
-const App = () => {
-    return (
-        <LoadingOverlay active={false} spinner text='Please Wait...'>
-            <div className="App">
-            </div>
-        </LoadingOverlay>
-    );
+import TopBar from './components/topbar';
+
+import './App.scss';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userData: []
+        }
+    }
+    componentDidMount() {
+        axios.get("http://localhost:8000/api/initialvalues").then((response) => {
+            this.setState(() => {
+                return {
+                    userData: response.data
+                }
+            })
+        })
+    }
+    render() {
+        return (
+            <LoadingOverlay active={false} spinner text='Please Wait...'>
+                <div className="App">
+                    <TopBar />
+                </div>
+            </LoadingOverlay>
+        );
+    }
 };
 
 export default App;
---
+++ client/src/components/topBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './topBar.scss';
+
+const TopBar = () => {
+    return (
+        <header className="header">
+            User Details
+        </header>
+    );
+}
+export default TopBar;
