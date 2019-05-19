fc1179f2b5bc7ea8930f1519db508a509edb0ca2
bhargav00 <bmaniar00@gmail.com>
2019-05-19 11:30:57 +0530

    basic server structure added

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+const routes = require('./routes');
+
+let app = express();
+app.server = http.createServer(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(morgan('dev'));
+
+app.use(cors());
+
+app.use('/', routes);
+
+app.server.listen(process.env.PORT || 8000, () => {
+    console.log(`Started on port ${app.server.address().port}`);
+});
